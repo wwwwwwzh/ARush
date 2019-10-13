a9775de5c948a3cabdf86915cb52296b50b5a37f
illuminator <2642503302@qq.com>
2019-10-13 21:37:24 +0800

    first commit, box created

---
+++ HyperCasual/Utilities.swift
@@ -0,0 +1,15 @@
+//
+//  Utilities.swift
+//  HyperCasual
+//
+//  Created by Yu Wang on 2019/10/13.
+//  Copyright © 2019 Yu Wang. All rights reserved.
+//
+
+import Foundation
+
+enum catagory:Int{
+    case player = 0
+    
+    case non = 100
+}
--- HyperCasual/ViewController.swift
+++ HyperCasual/ViewController.swift
@@ -12,64 +12,97 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
-    @IBOutlet var sceneView: ARSCNView!
+    var sceneView:ARSCNView = ARSCNView()
+    
+    var player:SCNNode!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
+        setUPSceneView()
+        setupCamera()
+        setUpPlayer()
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        //sceneView.debugOptions = [.showPhysicsShapes]
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+        configureARSession()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if let currentFrame = sceneView.session.currentFrame{
+            var translation = matrix_identity_float4x4
+            translation.columns.3.z = -0.5
+            player.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        }
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+}
+
+//MARK: viewDidLoad set up functions
+extension ViewController {
+    func setUPSceneView(){
+        view.addSubview(sceneView)
+        sceneView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            sceneView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            sceneView.topAnchor.constraint(equalTo: view.topAnchor),
+            sceneView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            sceneView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            ]
+        )
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+    func setupCamera() {
+        guard let camera = sceneView.pointOfView?.camera else {
+            fatalError("Expected a valid `pointOfView` from the scene.")
+        }
         
+        /*
+         Enable HDR camera settings for the most realistic appearance
+         with environmental lighting and physically based materials.
+         */
+        camera.wantsHDR = true
+        camera.exposureOffset = -1
+        camera.minimumExposure = -1
+        camera.maximumExposure = 3
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    func configureARSession() {
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.environmentTexturing = .automatic
+        configuration.wantsHDREnvironmentTextures = true
+        sceneView.session.run(configuration)
+    }
+    
+    //MARK: player set up
+    func setUpPlayer() {
+        setUpPlayerShape()
+        setUpPlayerPhysics()
+    }
+    
+    func setUpPlayerShape() {
+        player = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01))
+        player.name = "player"
+        let reflectiveMaterial = SCNMaterial()
+        reflectiveMaterial.lightingModel = .physicallyBased
+        reflectiveMaterial.metalness.contents = 1.0
+        reflectiveMaterial.roughness.contents = 0
+        player.geometry?.firstMaterial = reflectiveMaterial
+        sceneView.scene.rootNode.addChildNode(player)
+    }
+    
+    func setUpPlayerPhysics() {
+        player.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01), options: [SCNPhysicsShape.Option.type : SCNPhysicsShape.ShapeType.concavePolyhedron]))
+        player.physicsBody?.categoryBitMask = catagory.player.rawValue
+        player.physicsBody?.isAffectedByGravity = false
+//        player.physicsBody?.contactTestBitMask = catagory.upperChecker.rawValue | catagory.lowerChecher.rawValue
+//        player.physicsBody?.collisionBitMask = catagory.goal.rawValue
     }
 }
